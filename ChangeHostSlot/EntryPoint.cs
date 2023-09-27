d73a105e5e2143583ec93dbb2616024b754eb883
JarheadHME <JarheadHME@Gmail.com>
2023-09-27 16:23:22 -0400

    Add project files.

---
+++ ChangeHostSlot/EntryPoint.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using GTFO.API;
+using HarmonyLib;
+
+namespace ChangeHostSlot
+{
+    [BepInPlugin("HostSlot.GUID", "ChangeHostSlot", VersionInfo.Version)]
+    [BepInDependency("dev.gtfomodding.gtfo-api", BepInDependency.DependencyFlags.HardDependency)]
+    internal class EntryPoint : BasePlugin
+    {
+        public override void Load()
+        {
+            SlotConfig.CreateBind();
+            Logger.Info($"Patching on slot {SlotConfig.Slot} ({new string[] { "Pink/Woods", "Green/Dauda", "Blue/Hackett", "Purple/Bishop" }[SlotConfig.Slot]})");
+
+            Patch.ApplyNative();
+            Logger.Info("Loaded successfully");
+        }
+
+    }
+}
---
+++ ChangeHostSlot/SlotConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using BepInEx;
+using BepInEx.Configuration;
+
+namespace ChangeHostSlot
+{
+    public static class SlotConfig
+    {
+
+        public static int Slot;
+
+        public static void CreateBind()
+        {
+            BindSlot(new ConfigFile(Path.Combine(Paths.ConfigPath, "ChangeHostSlot.cfg"), true));
+            if (Slot < 0 || Slot > 3)
+            {
+                Logger.Error("Slot option not in valid range (0-3), defaulting to 0");
+                Slot = 0;
+            }
+        }
+
+        private static void BindSlot(ConfigFile config)
+        {
+            string section = "ChangeHostSlot";
+            string key = "Slot";
+            int DefaultVal = 0;
+            string description = "Slot to host in. 0 = Pink/Woods, 1 = Green/Dauda, 2 = Blue/Hackett, 3 = Purple/Bishop";
+            Slot = config.Bind<int>(new ConfigDefinition(section, key), DefaultVal, new ConfigDescription(description, null, Array.Empty<object>())).Value;
+        }
+    }
+}
